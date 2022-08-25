d8dbffc683645cfd4a5ccd73638aa69b162be074
Atennop <73060890+Atennop1@users.noreply.github.com>
2022-08-26 00:58:57 +0300

    Now On Github!

---
+++ VK Spam bot/VK Spam bot/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using VkNet;
+using VkNet.Model;
+using SpamBot.Senders;
+
+namespace SpamBot
+{
+    [Obsolete]
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string token = "vk1.a.UoHRbzoZYnMsMCriuEvX7fLT1EXyGyZ0QYIfHBhPt6yZqV6596mwQfnbuqEUibG24eQ6Qux6c2KXs1GkI2lx0qaNggQXs8DTmzyCJO9nzZ6_y37aLA6qeNqVje_kAm51CMdNDsM9qGpwocLfx0GndiQAESWFR3F9uXw55b_PPG8MfX0ZnxrQnFYW-z1rjCSy";
+            ulong id = 198111421;
+
+            VkApi api = new VkApi();
+            api.Authorize(new ApiAuthParams() { AccessToken = token });
+
+            DefaultMessageSender sender = new DefaultMessageSender(api);
+
+            Bot bot = new Bot(api, id, sender);
+            bot.StartListening();
+        }
+    }
+}
---
+++ VK Spam bot/VK Spam bot/Senders/IMessageSender.cs
@@ -0,0 +1,10 @@
+﻿using VkNet.Model;
+
+namespace SpamBot.Senders
+{
+    public interface IMessageSender
+    {
+        void SendMessage(string message, long? peerId, int count);
+        void SendMessage(Message message, long? peerId, int count);
+    }
+}
